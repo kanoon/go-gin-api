7abc9da2d9aac1eaddec086d68b3bb01d692b429
Marisa.cm <marisa.cm@gaitameasia.com>
2024-05-14 11:46:59 +0700

    DB updated

--- README.md
+++ README.md
@@ -1,25 +1,30 @@
-### Build DB
-
-```
-docker-compose down -v #if needed
-docker-compose up -d
-```
-
 ### Debug API
+
+Require to update `config/db.go` file and your DB should be ready.
 
 ```
 go run .\main.go
 ```
 
-### Build API
+### Run API (only) with docker container
 
 ```
 docker build -t my-go-app .
 docker run -p 8001:8001 my-go-app
 ```
 
-### Build DB and API
+### Run DB and API with docker container
 
 ```
 docker-compose up --build
 ```
+
+#### Site URLs
+
+- Docker container: http://localhost:8080/users
+- Debug: http://localhost:8001/users
+
+#### Database
+
+- Docker container: db,5432
+- Debug or pgAdmin: 127.0.0.1, 5444
--- config/db.go
+++ config/db.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"demo-api/models"
-	"fmt"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,17 +12,17 @@ var DB *gorm.DB
 func Connect() {
 
     // === Docker ===
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    dbName := os.Getenv("DB_NAME")
+    // dbHost := os.Getenv("DB_HOST")
+    // dbPort := os.Getenv("DB_PORT")
+    // dbUser := os.Getenv("DB_USER")
+    // dbPassword := os.Getenv("DB_PASSWORD")
+    // dbName := os.Getenv("DB_NAME")
 
-    dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        dbHost, dbPort, dbUser, dbPassword, dbName)
+    // dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+    //     dbHost, dbPort, dbUser, dbPassword, dbName)
 
     // === Debug ===
-    // dsn := "host=127.0.0.1 user=postgres password=test1234 dbname=postgres port=5444 sslmode=disable"
+    dsn := "host=127.0.0.1 user=postgres password=test1234 dbname=postgres port=5444 sslmode=disable"
     
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
